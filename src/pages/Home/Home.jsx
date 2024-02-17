d62df3edc333cc5b772cdd976d19af5da1568fe5
ajackwere <austinewere59@gmail.com>
2024-02-17 17:49:07 +0300

    added blog links to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,34 @@ import { Link } from "react-router-dom";
 import ChooseSection from "../../components/ChooseSection/ChooseSection";
 import StartCourseImg from "../../Utils/Images/startcourse.jpg";
 import Faq from "../../components/ChooseSection/Faq/Faq";
+import { Card } from "react-bootstrap";
+import Blog1Img from "../../Utils/Images/msomiCover.jpg";
+import Blog2Img from "../../Utils/Images/grad.jpg";
+import Blog3Img from "../../Utils/Images/lechall.jpg";
+
+const blogs = [
+  {
+    id: 1,
+    img: [Blog1Img],
+    title: "Blog 1",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error, voluptas",
+  },
+  {
+    id: 2,
+    img: [Blog2Img],
+    title: "Blog 2",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error, voluptas",
+  },
+  {
+    id: 3,
+    img: [Blog3Img],
+    title: "Blog 3",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error, voluptas",
+  },
+];
 
 export default function Home() {
   return (
@@ -47,10 +75,14 @@ export default function Home() {
             <div className="col-lg-5">
               <h2 className="text-capitalize"> 2024 start courses </h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, id!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias,
+                id!
               </p>
               <Link to="/courses">
-                <button type="button" className="btn btn-danger btn-lg mx-0 mx-sm-2 my-2 my-sm-0">
+                <button
+                  type="button"
+                  className="btn btn-danger btn-lg mx-0 mx-sm-2 my-2 my-sm-0"
+                >
                   Learn More!
                 </button>
               </Link>
@@ -62,7 +94,35 @@ export default function Home() {
         </div>
       </div>
       <div className="py-5">
-        <Faq/>
+        <Faq />
+      </div>
+      <div className="blog-section text-light py-5">
+        <div className="container d-flex flex-column align-items-center">
+          <h2 className="text-center text-capitalize mb-5">
+            Latest on our blog
+          </h2>
+          <div className="row g-4">
+            {blogs.map((blog) => (
+              <div key={blog.id} className="col-md-6 col-lg-4">
+                <Link to="/blog" className="text-decoration-none">
+                  <Card className="h-100 shadow scale-hover-effect">
+                    <Card.Img variant="top" src={blog.img} />
+                    <Card.Body className="p-md-5">
+                      <Card.Title>{blog.title}</Card.Title>
+                      <Card.Text>{blog.description}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Link>
+              </div>
+            ))}
+          </div>
+          <Link to="/blog">
+            <button type="button" className="btn btn-danger btn-lg mt-5">
+              {" "}
+              Read more blogs...
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
